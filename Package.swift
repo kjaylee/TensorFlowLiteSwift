e7d067647185ed0c63cb93289ca9c63ca6358ce0
hjlee <hjlee@aiforpet.com>
2025-06-12 16:34:24 +0900

    feat: 2.17.3

--- Package.swift
+++ Package.swift
@@ -4,13 +4,16 @@ import PackageDescription
 
 let package = Package(
     name: "TensorFlowLiteSwift",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "TensorFlowLiteSwift",
             targets: ["TensorFlowLite"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/kjaylee/TensorFlowLiteC.git", from: "2.17.1"),
+        .package(url: "https://github.com/kjaylee/TensorFlowLiteC.git", from: "2.17.3"),
     ],
     targets: [
         .target(
